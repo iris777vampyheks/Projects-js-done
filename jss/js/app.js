0ac611dab6fb869b2cba284ac2e497cb82d5d52c
Iris <bensassin6@gmail.com>
2023-07-02 16:46:20 +0100

    done100%

---
+++ jss/js/app.js
@@ -0,0 +1,93 @@
+// var myQueue = [];
+// myQueue.push(1);
+// myQueue.push(2);
+// myQueue.push(3);
+let something = [];
+something(1);
+something(2);
+something(3);
+something(4);
+something(5);
+something(6);
+something(7);
+something(8);
+something(9);
+something(10);
+something(11);
+something(12);
+something(13);
+something(14);
+something(15);
+something(16);
+something(17);
+something(18);
+something(19);
+something(20);
+something(21);
+something(21);
+something(22); 
+something(23);
+something(24);
+something(25);
+something(26);
+something(27);
+something(28);
+something(29);
+something(30);
+something(31);
+something(32);
+something(33);
+console.log(something.pop(2));
+let i = 1;
+let  mi = "me";
+let sfrth = "sfrtk";
+
+function(njl){
+  let money = $ 
+if money = validated {
+console.log("k,dcjdc,");
+}else{
+  console.log('hhhhhhhhhhh');
+}
+}
+
+// console.log(myQueue.shift());
+// console.log(myQueue.shift());
+// console.log(myQueue.shift());
+// //////////////////////////////////////////////////
+// function checkNumber(myNumber){
+//     if (myNumber === 42) {
+//         console.log('the word is correct');
+//     }else{
+//         console.log('the guessed word is incorrect');
+//     }
+// }
+// checkNumber(21 + 2 + 19);
+// checkNumber(7);
+// checkNumber(65);
+///////////////////////////////////
+// function multiplyNumber(num){
+// return num * 10;
+// }
+// console.log(multiplyNumber(40));
+//
+
+// function jma3lia(){
+// let a = 52;
+// let v = 55888;
+// let jam3 = a + v;
+// return jam3;
+// };
+// console.log(jma3lia());
+  // reverse a number
+function reverse(n) {
+    n = n + "";
+return n.split("").reverse().join("");
+}
+console.log(Number(reverse(4887879)));
+
+
+
+
+
+
